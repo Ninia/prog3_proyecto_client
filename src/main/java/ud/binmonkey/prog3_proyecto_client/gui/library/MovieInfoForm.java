745de9f5591b1127c2cfde7404e18d731add2fd7
mikelsr <mikelsolabarrieta@protonmail.com>
2017-06-14 13:37:20 +0200

    Enable attr edition in @MovieInfoForm

--- src/main/java/ud/binmonkey/prog3_proyecto_client/gui/library/MovieInfoForm.java
+++ src/main/java/ud/binmonkey/prog3_proyecto_client/gui/library/MovieInfoForm.java
@@ -9,6 +9,8 @@ import ud.binmonkey.prog3_proyecto_client.omdb.OmdbMovie;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -25,8 +27,13 @@ public class MovieInfoForm {
     public JLabel Plot;
     public JPanel infoPanel;
     public JTextArea plotTextArea;
-    public JButton saveButton;
+    public JButton confirmButton;
     public JPanel posterPanel;
+    public JPanel optionsPanel;
+    public JButton editButton;
+    public JButton saveChangesButton;
+    public JButton discardButton;
+    public JLabel infoLabel;
 
     public MovieInfoForm(OmdbMovie movie) {
 
@@ -37,6 +44,12 @@ public class MovieInfoForm {
         plotTextArea.setLineWrap(true);
         plotTextArea.setWrapStyleWord(true);
 
+        /* disable elements */
+        titleText.setEditable(false);
+        yearText.setEditable(false);
+        plotTextArea.setEditable(false);
+        saveChangesButton.setEnabled(false);
+        discardButton.setEnabled(false);
 
         /* Loads poster from IMDB */
         try {
@@ -51,6 +64,42 @@ public class MovieInfoForm {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        editButton.addActionListener(actionEvent -> {
+            saveChangesButton.setEnabled(true);
+            discardButton.setEnabled(true);
+            plotTextArea.setEditable(true);
+            yearText.setEditable(true);
+            titleText.setEditable(true);
+        });
+        saveChangesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    /* if empty */
+                    if (titleText.getText().replaceAll(" ", "").equals("")
+                            || yearText.getText().replaceAll(" ", "").equals("")
+                            || plotTextArea.getText().replaceAll(" ", "").equals("")) {
+                        infoLabel.setText("Fields cannot be empty");
+                        return;
+                    }
+                    movie.setTitle(titleText.getText());
+                    movie.setYear(yearText.getText());
+                    movie.setPlot(plotTextArea.getText());
+                    infoPanel.repaint();
+                    infoLabel.setText("Changed saved!");
+                } catch (Exception e) {
+                    infoLabel.setText("Error saving changes.");
+                }
+            }
+        });
+        discardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                titleText.setText(movie.getTitle());
+                yearText.setText(movie.getYear());
+                plotTextArea.setText(movie.getPlot());
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -83,44 +132,55 @@ public class MovieInfoForm {
         editPanel = new JPanel();
         editPanel.setLayout(new BorderLayout(0, 0));
         final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.setLayout(new GridLayoutManager(2, 3, new Insets(0, 0, 0, 0), -1, -1));
         editPanel.add(panel1, BorderLayout.SOUTH);
-        saveButton = new JButton();
-        saveButton.setText("Save");
-        panel1.add(saveButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        confirmButton = new JButton();
+        confirmButton.setText("Confirm");
+        panel1.add(confirmButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         panel1.add(spacer1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
-        panel1.add(spacer2, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        panel1.add(spacer2, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        infoLabel = new JLabel();
+        infoLabel.setText("");
+        panel1.add(infoLabel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         infoPanel = new JPanel();
-        infoPanel.setLayout(new GridLayoutManager(4, 4, new Insets(0, 0, 0, 0), -1, -1));
+        infoPanel.setLayout(new GridLayoutManager(5, 2, new Insets(0, 0, 0, 0), -1, -1));
         editPanel.add(infoPanel, BorderLayout.CENTER);
         idLabel = new JLabel();
         idLabel.setText("IMDB ID");
-        infoPanel.add(idLabel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        infoPanel.add(idLabel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         idText = new JTextField();
         idText.setEditable(false);
         idText.setText("id");
-        infoPanel.add(idText, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        infoPanel.add(idText, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         titleLabel = new JLabel();
         titleLabel.setText("Title");
-        infoPanel.add(titleLabel, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        infoPanel.add(titleLabel, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         titleText = new JTextField();
-        infoPanel.add(titleText, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        infoPanel.add(titleText, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         yearLabel = new JLabel();
         yearLabel.setText("Year");
-        infoPanel.add(yearLabel, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        infoPanel.add(yearLabel, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         yearText = new JTextField();
-        infoPanel.add(yearText, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        infoPanel.add(yearText, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         Plot = new JLabel();
         Plot.setText("Plot");
-        infoPanel.add(Plot, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        infoPanel.add(Plot, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         plotTextArea = new JTextArea();
-        infoPanel.add(plotTextArea, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
-        final Spacer spacer3 = new Spacer();
-        infoPanel.add(spacer3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        final Spacer spacer4 = new Spacer();
-        infoPanel.add(spacer4, new GridConstraints(2, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        infoPanel.add(plotTextArea, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        optionsPanel = new JPanel();
+        optionsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        infoPanel.add(optionsPanel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        editButton = new JButton();
+        editButton.setText("Edit");
+        optionsPanel.add(editButton);
+        saveChangesButton = new JButton();
+        saveChangesButton.setText("Save");
+        optionsPanel.add(saveChangesButton);
+        discardButton = new JButton();
+        discardButton.setText("Discard");
+        optionsPanel.add(discardButton);
         posterPanel = new JPanel();
         posterPanel.setLayout(new BorderLayout(0, 0));
         editPanel.add(posterPanel, BorderLayout.WEST);
